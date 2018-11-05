4cc6ed928b309aaa998d8a515f005574c0723223
Advait Kale <34103551+Advaitkale@users.noreply.github.com>
2018-11-05 10:11:48 +0530

    Update Sound.py

--- Sound.py
+++ Sound.py
@@ -1,3 +1,6 @@
+# Download the *.wav file to the python path before running the code
+
+# It is a simple code of Speech Recognition using the google Speech Recognition API.
 import speech_recognition as sr
 r = sr.Recognizer()
 with sr.AudioFile("hello.wav") as source:              # use "test.wav" as the audio source
